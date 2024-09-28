3243ec410668e7925ad18240fad929b32fdb3fde
Rita Rosa <ritapessoarosa@gmail.com>
2024-09-27 21:08:15 -0300

    Teste finalizado

--- config/config.js
+++ config/config.js
@@ -1,6 +1,30 @@
-const URL =
+const url =
   "https://wms-backend-g8-380719778871.southamerica-east1.run.app/api/Client";
 
-fetch(url)
-  .then((response) => response.json())
-  .then((nome) => console.log(nome));
+function fetchClientes(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((clientes) => {
+      clientes.forEach((cliente, index) => {
+        createCliente(cliente, index);
+        console.log(cliente);
+      });
+    });
+}
+
+// Cria a tabbela de clientes
+function createCliente(cliente) {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+          <td>${cliente.id}</td>
+          <td>${cliente.name}</td>
+          <td>${cliente.email}</td>
+          <td>${cliente.phone1}</td>
+          <td>${cliente.cnpjCpf}</td>
+          `;
+
+  document.querySelector("#contacts-table>tbody").appendChild(newRow);
+}
+
+// Chama a função
+fetchClientes(url);
